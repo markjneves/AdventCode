171d6b78803a6f6998fae0a743662e18b627353b
Mark Neves <markjneves@gmail.com>
2025-12-02 10:53:31 -0600

    Logger updates

--- AdventCode/Logger.cs
+++ AdventCode/Logger.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace AdventCode
 {
-    
+    public enum LogLevel
+    {
+        Trace, Info, Warn, Error
+    }
     public static class Logger
     {
-        static StreamWriter sw_Writer;
+        private static StreamWriter sw_Writer;
+        private static LogLevel ll_WriteLevel = LogLevel.Trace;
+        private static LogLevel ll_ConsoleLevel = LogLevel.Trace;
+
+        public static LogLevel WriteLevel
+        {   get { return ll_WriteLevel; }
+            set { ll_WriteLevel = value; } }
+        public static LogLevel ConsoleLevel
+        {
+            get { return ll_ConsoleLevel; }
+            set { ll_ConsoleLevel = value; }
+        }
 
         public static void SetFile(string filename)
         {
@@ -28,6 +42,13 @@ namespace AdventCode
         {
             Console.WriteLine(message);
             sw_Writer.WriteLine(message);
+        }
+        public static void Log(string message, LogLevel level)
+        {
+            if(level >= ll_ConsoleLevel)
+                Console.WriteLine(message);
+            if (level >= ll_WriteLevel)
+                sw_Writer.WriteLine(message);
         }
     }
 }
