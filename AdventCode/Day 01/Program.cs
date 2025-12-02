462c0adf5af4643a3061c75a505c406bc7e05e76
Mark Neves <markjneves@gmail.com>
2025-12-02 09:10:46 -0600

    Logger addition

---
+++ AdventCode/Day 01/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventCode;
+
+Console.WriteLine("Hello, World!");
+String[] str_Input = File.ReadAllLines("C:\\Users\\Mark\\source\\repos\\AdventCode\\AdventCode\\Day 01\\input.txt");
+LockCodeHandler lch =  new LockCodeHandler();
+String str_Output = "";
+Logger.SetFile("C:\\Users\\Mark\\source\\repos\\AdventCode\\AdventCode\\Day 01\\output.txt");
+foreach (String str in str_Input)
+{
+    Logger.ConsoleLog("---Operation---");
+    Logger.ConsoleLog(str);
+
+    lch.Rotate(str);
+
+    Logger.ConsoleLog(lch.GetDigit().ToString());
+    Logger.ConsoleLog(lch.GetZeroCounter().ToString());
+    Logger.ConsoleLog("");
+}
+
+Logger.ConsoleLog("--Complete--");
+Logger.ConsoleLog("Zero Count: " + lch.GetCounter(0).ToString());
+Logger.ConsoleLog("New Zero Count: " + lch.GetZeroCounter().ToString());
+Logger.CloseFile(); 
---
+++ AdventCode/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode
+{
+    
+    public static class Logger
+    {
+        static StreamWriter sw_Writer;
+
+        public static void SetFile(string filename)
+        {
+            if(File.Exists(filename))
+                File.Delete(filename);
+            sw_Writer = new StreamWriter(filename);
+        }
+        public static void CloseFile()
+            { sw_Writer.Close(); }
+
+        public static void Log(string message)
+        {
+            sw_Writer.WriteLine(message);
+        }
+        public static void ConsoleLog(string message)
+        {
+            Console.WriteLine(message);
+            sw_Writer.WriteLine(message);
+        }
+    }
+}
